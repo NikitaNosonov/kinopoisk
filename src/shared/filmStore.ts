afc510c05ed40c69b297040648a2d0ac0da7f111
nikitanosonov <nikitanosonov93@gmail.com>
2025-07-11 14:25:23 +0300

    mobx

---
+++ src/shared/filmStore.ts
@@ -0,0 +1,55 @@
+import {action, makeAutoObservable, runInAction} from "mobx";
+import {Film} from "./typesData";
+
+class FilmStore {
+    public film: Film[] = [];
+    public filmById?: Film;
+
+    constructor() {
+        makeAutoObservable(this)
+    }
+
+    fetchFilm = action(async () => {
+        const response = await fetch('https://246b98815ac8edb9.mokky.dev/listFilms', {
+            headers: {
+                "Authorization": `Bearer ${localStorage.getItem('token')}`
+            }
+        });
+        const json: Film[] = await response.json();
+
+        runInAction(() => {
+            this.film = json;
+        });
+    })
+
+    fetchFilmId = action(async (id: number | null) => {
+        const response = await fetch(`https://246b98815ac8edb9.mokky.dev/listFilms/${id}`, {
+            headers: {
+                "Authorization": `Bearer ${localStorage.getItem('token')}`
+            }
+        });
+        const json: Film = await response.json();
+
+        runInAction(() => {
+            this.filmById = json;
+        });
+    })
+
+    fetchEditFilm = action(async (id: number | null, editedFilm: Film) => {
+        const response = await fetch(`https://246b98815ac8edb9.mokky.dev/listFilms/${id}`, {
+            method: "PATCH",
+            headers: {
+                "Authorization": `Bearer ${localStorage.getItem('token')}`,
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(editedFilm),
+        });
+        const json: Film = await response.json();
+
+        runInAction(() => {
+            this.filmById = json;
+        })
+    })
+}
+
+export default new FilmStore()
---
+++ src/shared/modalForm/ModalFormFilm.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {TextField} from "@mui/material";
+
+const ModalFormFilm = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default ModalFormFilm;
